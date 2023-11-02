36839369eb1411d20a9480aa616f8683df01269d
MohamedSalahFCI <mohamed.sallah147@gmail.com>
2023-11-02 16:46:02 +0200

    comment all file

--- post_deployment.sh
+++ post_deployment.sh
@@ -2,6 +2,13 @@
 echo ‘installing requirements…’ 
 npm install
 echo ‘start server…’
-forever start -c “npm start” ./
+service_name="hello-api"
+if pm2 list | grep -q "$service_name"; then
+  echo "PM2 service '$service_name' is running. Restarting..."
+  pm2 restart "$service_name"
+else
+  echo "PM2 service '$service_name' is not running. Starting..."
+  pm2 start npm --name "$service_name" -- start
+fi
 echo ‘started server. ending SSH session..’
 exit
