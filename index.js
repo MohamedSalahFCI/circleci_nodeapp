dfee5aeacb834f3383b63eefb5856f812e41f349
MohamedSalahFCI <mohamed.sallah147@gmail.com>
2023-11-06 14:34:26 +0200

    check from another machine

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World ya salaaaaaah! from main");
+    response.end("Hello World ya salaaaaaah! from machine 164.90.217.117");
 
 });
 
